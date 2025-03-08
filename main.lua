b9fed631ec6f341c71a41c682efa002211f4476b
Joshua S. Day <haxmeister@hotmail.com>
2025-03-08 03:11:46 -0600

    Revert "proper aliasing and ui actions"
    This reverts commit 358213c6a74f9a623f7bdeace175fc5d6f5dfdba.

--- main.lua
+++ main.lua
@@ -2,43 +2,59 @@
 
 Author: Joshua S. Day (haxmeister)
 
-Purpose: This plugin provides event hooks to simplify a private chat plugin
-         See the readme file that should have accompanied this plugin for details
+Purpose: This plugin provides access to the flyboy server API. Using this
+         Connection, it provides shared data between players using
+         the plugin.
 
+Flyboy plugin enables sharing of the follow types of data, assuming it
+has been provided by one of it's users:
+
+    1. Location of other players you and others have seen in real time.
+    2. Locations asteroids with ore data that has been scanned by you or others.
+    3. Location of unrats and unrat dent that has been seen by you or others.
+    4. Location of queens that have been seen by you or others.
+    5. Location of Leviathon that has been seen by you or others.
+    6. A private chat channel that is NOT monitored by other players or game
+       administrators.
 
 --]]
 
--- The table that will contain the AllianceChat plugin
-AllianceChat = AllianceChat or {}
+-- The table that will contain the flyboy plugin
+Flyboy = Flyboy or {}
 
-AllianceChat.Settings = dofile "settings.lua"
-AllianceChat.Commands = dofile "commands.lua"
-AllianceChat.UI = dofile "ui.lua"
+-- The IP address or url of the server
+Flyboy.server = "192.168.0.30"
 
--- event hooks
-function AllianceChat.recv (_,data)
-    local msg = AllianceChat.Settings.promptColor
-    msg = msg..AllianceChat.Settings.channelName
-    msg = msg..data.factionColor
-    if (data.guild ~= "")then
-        msg = msg.."["..data.guild.."] "
-    end
-    msg = msg..data.name..": "
-    msg = msg..AllianceChat.Settings.msgColor
-    msg = msg..data.msg
+-- The port to use on the server
+Flyboy.port = 3000
 
-    print (msg)
-end
+-- Functions to load and save settings and update the running table
+Flyboy.Settings = dofile "settings.lua"
 
--- send the user's message to the event for other plugins to process
-function AllianceChat.send(msg)
-    local data = {
-        name = GetPlayerName(),
-        guild = GetGuildTag() or "",
-        factionColor = rgbtohex(FactionColor_RGB[tonumber(GetPlayerFaction())]) or '127FFFFFF',
-        msg = msg,
-    }
-    ProcessEvent("ALLIANCE_CHAT_SEND", data)
-end
+-- The name of the system notes file that will store your settings
+Flyboy.Settings.fileID = "flyboy"
 
-RegisterEvent(AllianceChat.recv, "ALLIANCE_CHAT_RECV")
+-- Loading previous flyboy settings from the fileID given
+Flyboy.Settings:Load()
+
+-- Functions to manage the TCP connection to the server
+Flyboy.Conn = dofile "connectivity/connection.lua"
+
+-- Game events to watch and FB (flyboy) custom events to trigger and watch,
+-- all events are registered in this file as well
+Flyboy.Events = dofile "events.lua"
+
+-- Commands that can be executed in the VO chat window during gameplay
+Flyboy.Commands = dofile "commands.lua"
+
+-- Load MDI-targetlist integrations
+Flyboy.MDItargetlist = dofile "integrations/targetlist.lua"
+
+--Flyboy.ui       = dofile "ui/spots.lua"
+
+
+
+
+
+
+
